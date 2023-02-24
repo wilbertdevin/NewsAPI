53e7d1354787d3500bb275657e0d42ab594e3bef
wilbertdevin <wilbertdevin888@gmail.com>
2023-02-24 10:19:20 +0700

    First commit

---
+++ NewsAPI/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  NewsAPI
+//
+//  Created by Wilbert Devin Wijaya on 24/02/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
